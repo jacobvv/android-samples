db35cd886844738fafe4ac6e33840934c9a05134
Jacob Yin <jacobyinvv@gmail.com>
2019-04-20 00:09:08 +0800

    Add media loader for sample of cursor recycler adapter.

---
+++ app/src/main/java/org/jacobvv/androidsamples/recycler/cursor/MediaLoader.java
@@ -0,0 +1,46 @@
+package org.jacobvv.androidsamples.recycler.cursor;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.MediaStore;
+import android.support.v4.content.CursorLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jacob
+ * @date 19-4-19
+ */
+public class MediaLoader extends CursorLoader {
+
+    private static final String[] PROJECTION = {
+            MediaStore.Files.FileColumns._ID,
+            MediaStore.MediaColumns.DISPLAY_NAME,
+            MediaStore.MediaColumns.DATA
+    };
+
+    private static final String SELECTION_TYPE = MediaStore.Files.FileColumns.MEDIA_TYPE + "=?";
+    private static final String SELECTION_SIZE = MediaStore.MediaColumns.SIZE + ">? OR " +
+            MediaStore.MediaColumns.SIZE + " IS NULL";
+    private static final String ORDER = "datetaken DESC";
+
+    private MediaLoader(Context context, String selection, String[] selectionArgs) {
+        super(context, MediaStore.Files.getContentUri("external"), PROJECTION,
+                selection, selectionArgs, ORDER);
+    }
+
+    static CursorLoader newInstance(Context context) {
+        String selection = SELECTION_TYPE + " AND (" + SELECTION_SIZE + ")";
+        String[] selectionArgs = new String[]{};
+        List<String> args = new ArrayList<>(1);
+        args.add(String.valueOf(MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE));
+        args.add(String.valueOf(1000));
+        return new MediaLoader(context, selection, args.toArray(selectionArgs));
+    }
+
+    @Override
+    public Cursor loadInBackground() {
+        return super.loadInBackground();
+    }
+}
---
+++ app/src/main/java/org/jacobvv/androidsamples/recycler/cursor/MediaRepository.java
@@ -0,0 +1,78 @@
+package org.jacobvv.androidsamples.recycler.cursor;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+
+/**
+ * @author Jacob
+ * @date 19-4-19
+ */
+public class MediaRepository {
+
+    private static final int LOADER_ID_MEDIA = 1000;
+
+    private Context mContext;
+    private LoaderManager mLoaderManager;
+
+    public MediaRepository(FragmentActivity activity) {
+        mContext = activity.getApplicationContext();
+        mLoaderManager = activity.getSupportLoaderManager();
+    }
+
+    public void loadMedia(@NonNull MediaCallbacks callbacks) {
+        Bundle args = new Bundle();
+        if (mLoaderManager.getLoader(LOADER_ID_MEDIA) != null) {
+            mLoaderManager.restartLoader(LOADER_ID_MEDIA, args, new MediaLoaderCallbacks(callbacks));
+        } else {
+            mLoaderManager.initLoader(LOADER_ID_MEDIA, args, new MediaLoaderCallbacks(callbacks));
+        }
+    }
+
+    public interface MediaCallbacks {
+        /**
+         * Called when media load complete and return media item data.
+         *
+         * @param cursor media items cursor of given bucket id load from db.
+         */
+        void onMediaLoaded(Cursor cursor);
+
+        /**
+         * Called when a previously data loaded is being reset, and thus
+         * making its data unavailable.  The application should at this point
+         * remove any references it has to the Loader's data.
+         */
+        void onMediaReset();
+    }
+
+    private class MediaLoaderCallbacks implements LoaderManager.LoaderCallbacks<Cursor> {
+
+        private MediaCallbacks mCallbacks;
+
+        private MediaLoaderCallbacks(MediaCallbacks callbacks) {
+            mCallbacks = callbacks;
+        }
+
+        @NonNull
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+            return MediaLoader.newInstance(mContext);
+        }
+
+        @Override
+        public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+            mCallbacks.onMediaLoaded(data);
+        }
+
+        @Override
+        public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+            mCallbacks.onMediaReset();
+        }
+    }
+
+}
